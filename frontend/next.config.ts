6b5b8a41fab189712b4f8f9a641ff15677d0f883
Chaitanya-Keyal <66475772+Chaitanya-Keyal@users.noreply.github.com>
2025-02-15 09:35:02 +0530

    fix: use vercel as a reverse proxy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "build",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://biohue.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
